6ff04ea3111d6c91a6d22be25b8cf53a2771e0d1
rdpqm2 <34043524+rdpqm2@users.noreply.github.com>
2018-10-19 17:52:48 -0500

    Add files via upload

---
+++ Colors With Navigation/Colors/Color.swift
@@ -0,0 +1,14 @@
+//
+//  Color.swift
+//  Colors
+//
+//  Created by Rahil Patel on 10/19/18.
+//  Copyright © 2018 array. All rights reserved.
+//
+
+import UIKit
+
+struct Color {
+    let name: String
+    let uiColor: UIColor
+}
